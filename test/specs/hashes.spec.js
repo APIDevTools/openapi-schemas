b4a570056234dfe8fef83765ba0ac6408f2fc4ac
Hans Klunder <hans.klunder@gmail.com>
2021-04-18 13:35:10 +0200

    Added support for OpenApi3.1

--- test/specs/hashes.spec.js
+++ test/specs/hashes.spec.js
@@ -15,6 +15,7 @@ describe("Schema hashes", () => {
     openapiV1: "fedbb966f17e54afd8ddcb87cb12d4b42715fbb493a66581861cf2f8aa4d6b10",
     openapiV2: "220e4d47e8dbd8f72a612898429b970bfd652e663569f7202002ceb9767e033a",
     openapiV3: "12ba334b36afd9443f1a04cf7fccf09b4a8059d1b7f851faebe91cf60d01f633",
+    openapiV31: "549386120954be0bdd3778c72ceb376becb83489b4143fe9c26b0b65ebe9891e",
   };
 
   for (let [version, hash] of Object.entries(hashes)) {
